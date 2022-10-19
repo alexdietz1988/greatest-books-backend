a37dcf1dc36d50947fd82cd0c5ed7ce4e02d5e79
alexdietz1988 <alex.dietz@gmail.com>
2022-10-19 11:07:59 -0400

    Initialize signup route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcryptjs')
 const db = require('../db')
 
 router.post('/signup', async (req, res) => {
     try {
-        let user = req.body.user
-        // const foundUser = await db.User.exists({ user })
-        if (foundUser) return res.json({ success: false, error: 'user already exists' })
+        const text = 'INSERT INTO auth(email) VALUES ($1) RETURNING *'
+        const values = [req.body.email]
 
-        const salt = await bcrypt.genSalt(12)
-        const hash = await bcrypt.hash(req.body.password, salt)
-        req.body.password = hash
+        const res = await db.query(text, values)
+        console.log(res.rows[0])
 
-        // await db.User.create(req.body)
-        // await db.Basics.create({ user })
         return res.json({ success: true })
-
     } catch (error) {
         console.log(error)
         req.error = error
