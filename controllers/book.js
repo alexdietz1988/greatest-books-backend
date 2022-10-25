172bc4eaabb25865b908e761b3d09e3a00e9bd4a
alexdietz1988 <alex.dietz@gmail.com>
2022-10-25 09:05:19 -0400

    Refactor book route

--- controllers/book.js
+++ controllers/book.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 const db = require('../db')
 
-router.get('/fiction', async (req, res) => {
+// Get all books of specified genre and date range
+router.get('/', async (req, res) => {
     try {
-        const response = await db.query(`SELECT * FROM book WHERE genre = 'fiction';`, [])
+        const response = await db.query(
+            `SELECT * FROM book WHERE genre = $1 AND publication_date BETWEEN CAST($2 AS SMALLINT) AND CAST($3 AS SMALLINT);`,
+            [req.body.genre, req.body.start, req.body.end]
+        )
         return res.json(response.rows)
     } catch (error) {
         console.log(error)
@@ -12,14 +16,6 @@ router.get('/fiction', async (req, res) => {
     }
 })
 
-router.get('/nonfiction', async (req, res) => {
-    try {
-        const response = await db.query(`SELECT * FROM book WHERE genre = 'nonfiction';`, [])
-        return res.json(response.rows)
-    } catch (error) {
-        console.log(error)
-        return res.json(error)
-    }
-})
+
 
 module.exports = router
